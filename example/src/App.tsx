b517ba95badbd7c167352c8031de123f8483c3b3
Sujan Tamang <everestgaming3@gmail.com>
2022-09-27 13:41:11 +0545

    refactor: cleanup autogenerated codes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import SayHello from "react-complex-state";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
@@ -17,18 +14,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <SayHello name="Kenshein" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SayHello name="Kenshein" />
     </div>
   );
 }
